f97a5d8e2c8cde9fd2e8c53b7d38cce4bd3e28ee
freddie <freddielutu@gmail.com>
2020-10-18 11:48:02 +0100

    completed etl

--- pipeline/load.py
+++ pipeline/load.py
@@ -1,7 +1,6 @@
-from extract import read_unf_csv
-from transform import transform_rows, transform_row
-from transform import data
-from persistance import connection, query, update
+from pipeline.extract import read_unf_csv
+from pipeline.transform import transform_rows, transform_row, data
+from pipeline.persistance import connection, query, update
 import uuid
 
 db = connection("CapsuleCorp")
@@ -20,16 +19,15 @@ def load_location_row(row):
         l_id = str(uuid.uuid4())
         update(db, updateDbQuery, (l_id, location_name))
     else:
-        l_id = check
+        l_id = check[0][0]
     
     return l_id
 
-def load_transaction_row(row):
+def load_transaction_row(row, l_id):
 
-    updateDbQuery = f""" INSERT INTO Transaction (id, date, location_id, payment_type, total) VALUES (%s, %s, %s, %s, %s)"""
+    updateDbQuery = f""" INSERT INTO Transaction (id, date_time, l_id, payment_type, total) VALUES (%s, %s, %s, %s, %s)"""
     tsac_id = str(uuid.uuid4())
     date_time = row[0]
-    location = row[1] 
     payment_type = row[3]
     total = row[4]
 
@@ -41,7 +39,7 @@ def load_product_row(row):
     p_id = None
     checkDbQuery = f"""SELECT id FROM Product WHERE size = '{product['size']}' AND name = '{product['name']}' AND price = '{float(product['price'])}'"""
     updateDbQuery = """INSERT INTO Product ( id, size, name, price) VALUES (%s, %s, %s, %s)"""
-
+    id_dict = {}
     basket = row[2]
 
     for product in basket:
@@ -50,23 +48,39 @@ def load_product_row(row):
             p_id = str(uuid.uuid4())
             update(db, updateDbQuery, (p_id, product['size'], product['name'],product['price']))
         else:
-            p_id = check
+            p_id = check[0][0]
+        prod = {p_id: product['price']}
+        id_dict.update(prod)
         
-    return p_id
+    return id_dict
         
 
    
 
 
 
-#def load_orders_row(row):
+def load_orders_row(d_time, tsac_id, p_id, price):
+    updateDbQuery = """INSERT INTO Orders (date_time, tsac_id, prod_id, price) VALUES (%s, %s, %s, %s)"""
+
+    update(db, updateDbQuery, (d_time, tsac_id, p_id, price))
+
+
+
 
 
 
 def load_by_row(t_data):
 
     for row in t_data:
+
+        date = row[0]
         l_id = load_location_row(row)
-        tsac_id = load_transaction_row(row)
+        tsac_id = load_transaction_row(row, l_id)
+        id_list = load_product_row(row)
+
+        for p_id, price in id_list.values():
+
+            load_orders_row(date, tsac_id, p_id, price)
 
         
+
