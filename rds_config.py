bbe1fc68f9195e84bd58ad0fa10325ebc52e6301
Charlie D <58493339+ChazzaD@users.noreply.github.com>
2020-10-22 14:48:38 +0100

    cleanup

--- rds_config.py
+++
@@ -1,6 +0,0 @@
-#config file containing credentials for RDS MySQL instance
-rds_endpoint  = os.environ.get('RDS_endpoint')
-db_user = os.environ.get('DB_USER')
-db_password = os.environ.get('DB_PASS')
-db_name = "Transactions_Prod" 
-db_con = None
