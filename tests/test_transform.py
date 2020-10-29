09bfb7b8f76827e509ec0aae380c3b3cd846681a
adion123 <70028378+adion123@users.noreply.github.com>
2020-10-29 14:15:41 +0000

    Created test class

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -3,4 +3,8 @@ from src.transform import transform_row, transform_rows
 import time
 import numpy as np
 
-class TestTransfom(unittest.Testcase)
+class TestTransfom(unittest.Testcase):
+    def test_transform_row(self):
+        pass
+
+
